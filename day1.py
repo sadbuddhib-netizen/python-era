0648bea7243a00740e337987a2487fa4a41ab370
sadbuddhib-netizen <sadbuddhib@gmail.com>
2026-06-07 21:52:38 +0530

    Add files via upload
    # Day 1 - Python Basics 
    
    Today I started my Python journey and learned the fundamentals of programming.
    
    Topics covered:
    • Variables and Data Types
    • User Input
    • Type Casting
    • Arithmetic Operators
    • Basic Python Syntax
    
    Built a few simple programs to practice these concepts. Looking forward to learning more every day!

---
+++ day1.py
@@ -0,0 +1,31 @@
+
+
+'''a = 10
+b = 20
+print (a+b)
+
+
+l= 30
+b =40
+area = l*b
+print (area)
+
+a=10
+b=20
+a,b =b,a
+print(a,b)
+
+p =1000
+r =5
+t =2
+si =(p*r*t)/100
+print(si)
+
+
+c = 100
+f = (c*9/5)+32
+print(f)
+
+'''
+
+
