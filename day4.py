abeae20bb3323a984d6828be94cf31b68ecca5f9
sadbuddhib-netizen <sadbuddhib@gmail.com>
2026-06-11 18:41:51 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,69 @@
+for i in range(3):
+    print("hello")
+for i in range(1,6):
+    print("hello")
+for i in range(10):
+    print(i)
+
+num = int(input("enter a number"))
+for i in range(1,11):
+    print(num,"*",i,"=",num*i)
+
+total = 0
+for i in range (1,11):
+      total = total + i
+      print(total)
+
+for i in range(0,101,2):
+    print(i)
+
+for i in range (1,101,2):
+    print("coun is even number",i)
+
+
+for i in range (1,6):
+    print("x" *i)
+
+for i in range (1,5):
+    print("x" * i)
+
+for i in range (1,21):
+    print("print no from 1 to 20",i)
+ 
+for i in range(1,51,2):
+    print("odd number from 1 to 50",i)
+
+num = 7
+for i in range(1,11):
+   print(num,"X",i,"=",num*i)
+
+total = 0
+for  i in range (1,101):
+    total = total + i
+    print("sum of 1 to 100" , total)
+
+for i in  range (1,101):
+  if  i % 5 == 0:
+    print(i)
+    
+
+
+
+
+
+
+    
+
+
+
+
+    
+
+
+
+
+
+        
+
+
+
