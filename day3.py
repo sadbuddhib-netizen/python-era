e9935f253fef929a6bf83bf8003d9e3f655dacec
sadbuddhib-netizen <sadbuddhib@gmail.com>
2026-06-11 18:02:47 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,41 @@
+'''a = [1,3,2]           
+print(a) 
+
+b  = (1,2,3)
+print(b) 
+
+names = ["apple","banana","orange"]
+print(names)
+
+fruits = ["orange","mango","strawberry"]
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[-2])
+fruits.append("kiwi")
+print(fruits)
+fruits.remove("mango")
+print(fruits)
+
+fruits = ["Apple", "Mango", "Banana"]
+
+print(len(fruits))
+
+fruits =["apple","mango","litchi"]
+for fruit in fruits: 
+     print(fruit) 
+
+
+marks = [20,30,40,50,60,70,100]
+print("largest number", max(marks))
+print("smallest number",min(marks))
+print("total marks",sum(marks))
+print("average marks",sum(marks)/len(marks)) '''
+
+marks = [100,30,40,20,90]
+marks.sort()
+print(marks)
+print(marks[-2])
+
+
+
