a195fea7b10c101fb39d66a3cdd0c31d685f3bd9
sadbuddhib-netizen <sadbuddhib@gmail.com>
2026-06-11 20:57:28 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,46 @@
+'''name = "string"
+print(name)
+
+sub ="python"
+print(sub[0:5])
+
+name = "sabu"
+print(name[1])
+
+name = "sabu"
+print(name.upper())
+fruit ="BANANA"
+print(fruit.lower())
+print(len(fruit))
+
+text = "my name is sabu"
+print("sadbuddhi" in text)
+print ("sabu" in text)
+
+name = "sabu"
+surname = "bhaigade"
+print (name,surname)
+
+word = input("enter a word:")
+if word == word[:: -1]:
+    print("word is palindrom")
+else:
+    print("word is not palindrom")
+
+word = "madam"
+print(word[:: -1])
+
+name = "sabu"
+print(name[0])
+print(name[3])
+print(name.upper())
+print(len(name))'''
+
+word = input("enter a word:")
+if word == word[::-1]:
+    print("word is palindrom")
+else:
+    print("word is not palindrom")   
+
+
+
