2f703f33b6e53d2961f2beae74febf4434ee807b
sadbuddhib-netizen <sadbuddhib@gmail.com>
2026-06-14 13:10:17 +0530

    Add files via upload

---
+++ day8.py
@@ -0,0 +1,30 @@
+'''number = [12,34,56]
+number.append(40)
+print(number)
+
+numbers = (20,30,50,60)
+print(numbers[0])
+print(numbers[1])
+print(numbers[3])
+print()
+print(numbers)
+
+maths ={30,40,90}
+maths.remove(40)
+print(maths)'''
+
+
+sabu = [20,60,50,60]
+print(sabu[0])
+print(sabu[3])
+
+om = {20,40,30,30,40,20,100,400}
+om.add(101)
+print(om)
+
+numbers = {10, 20, 30, 20, 40, 30, 50}
+print("highest:", max(numbers))
+print("lowest:", min(numbers))
+print("total:", sum(numbers))
+print("average:",sum(numbers)/len(numbers))
+
